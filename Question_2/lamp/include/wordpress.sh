e8ac74c57f40bb81969ad5d370d36c0010bcbc3f
Kirit Devda <kiritdevda15@gmail.com>
2019-11-19 04:41:11 +0800

    Added Semicolon to php file

--- Question_2/lamp/include/wordpress.sh
+++ Question_2/lamp/include/wordpress.sh
@@ -13,7 +13,13 @@ config_wordpress(){
     cd ${web_root_dir}/
     cp wp-config-sample.php wp-config-sample.php.bkp
     mv wp-config-sample.php wp-config.php
-    sed  -i "s/^define(.*'DB_PASSWORD'.*/define( \x27DB_PASSWORD\x27, \x27${mysql_word_press_password}\x27 )/" wp-config.php
-    sed  -i "s/^define(.*'DB_USER'.*/define( \x27DB_USER\x27, \x27${mysql_word_press_user}\x27 )/" wp-config.php
-    sed  -i "s/^define(.*'DB_NAME'.*/define( \x27DB_NAME\x27, \x27${mysql_word_press_db}\x27 )/" wp-config.php
+    sed  -i "s/^define(.*'DB_PASSWORD'.*/define( \x27DB_PASSWORD\x27, \x27${mysql_word_press_password}\x27 );/" wp-config.php
+    sed  -i "s/^define(.*'DB_USER'.*/define( \x27DB_USER\x27, \x27${mysql_word_press_user}\x27 );/" wp-config.php
+    sed  -i "s/^define(.*'DB_NAME'.*/define( \x27DB_NAME\x27, \x27${mysql_word_press_db}\x27 );/" wp-config.php
+        if check_sys packageManager apt; then
+            error_detect_depends "apt-get -y install dos2unix"
+        elif check_sys packageManager yum; then
+            error_detect_depends "yum -y install dos2unix"
+	fi
+dos2unix wp-config.php
 }
